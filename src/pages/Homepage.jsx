f873ae0022ebebc353b87e6e3fc809a25dc3b671
Atena Razm <atenarazm@gmail.com>
2024-08-02 11:41:20 -0700

    initial commit, NavBar routing defined

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import PageNav from '../components/PageNav';
+
+function Homepage() {
+  return (
+    <div>
+      {' '}
+      <PageNav />
+      <h1>Homepage</h1>
+    </div>
+  );
+}
+
+export default Homepage;
