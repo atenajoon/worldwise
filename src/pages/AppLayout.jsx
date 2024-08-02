117c7158be603e8bd19ea3cca4f51db377a1deda
Atena Razm <atenarazm@gmail.com>
2024-08-02 14:34:10 -0700

    all component files imported from Starter

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,13 @@
-import AppNav from '../components/AppNav';
+// import AppNav from '../components/AppNav';
+import Sidebar from '../components/Sidebar';
+import styles from './AppLayout.module.css';
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>App Layout</p>
+    <div className={styles.app}>
+      <Sidebar />
+      {/* <AppNav />
+      <p>App Layout</p> */}
     </div>
   );
 }
