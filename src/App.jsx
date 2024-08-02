67888f98fb84acadb16bd84d91d178e358bc1e55
Atena Razm <atenarazm@gmail.com>
2024-08-02 14:14:25 -0700

    login btn style

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='login' element={<Login />} />
-
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
