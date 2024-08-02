47cf52a718c9223dbd4a25a5b181035db758ae03
Atena Razm <atenarazm@gmail.com>
2024-08-02 14:12:07 -0700

    PageNav added to all componenets, Logo got a link to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
+import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 
@@ -10,6 +11,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path='login' element={<Login />} />
+
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
         <Route path='app' element={<AppLayout />} />
