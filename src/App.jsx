6f1f3256c54a973ad878dd5f3df731484549a049
Atena Razm <atenarazm@gmail.com>
2024-08-06 16:26:08 -0700

    City component initial file added, bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import City from './components/City';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -52,6 +53,7 @@ function App() {
             path='cities'
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path='cities/:id' element={<City />} />
           <Route path='form' element={<p>form</p>} />
         </Route>
         <Route path='*' element={<PageNotFound />} />
