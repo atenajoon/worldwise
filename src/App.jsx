58a689f57b33b0f12c2b47248c6eab179a8c9012
Atena Razm <atenarazm@gmail.com>
2024-08-12 09:52:58 -0700

    Navigate on index route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Homepage from './pages/Homepage';
 import Product from './pages/Product';
@@ -42,10 +42,7 @@ function App() {
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to='cities' />} />
           <Route
             path='countries'
             element={<CountryList cities={cities} isLoading={isLoading} />}
