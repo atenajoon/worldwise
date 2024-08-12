69bcf8ba73a0aa6b28ed7a99cd42609329078dcd
Atena Razm <atenarazm@gmail.com>
2024-08-12 09:45:45 -0700

    button component created, added to form with programmatic navigation

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import styles from './button.module.css';
+
+export default function Button({ children, onClick, type }) {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
+}
