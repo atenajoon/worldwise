f0844a19792577faf13a066cdd1aa114f3bbf227
Atena Razm <atenarazm@gmail.com>
2024-08-16 14:11:30 -0700

    fixing getCity bug with useCallback

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useEffect, useContext, useReducer } from 'react';
+import {
+  createContext,
+  useEffect,
+  useContext,
+  useReducer,
+  useCallback,
+} from 'react';
 
 const CitiesContext = createContext();
 const BASE_URL = 'http://localhost:9000';
@@ -67,21 +73,24 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
 
-    dispatch({ type: 'loading' });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: 'city/deleted', payload: data });
-    } catch (err) {
-      dispatch({
-        type: 'cities/loaded',
-        payload: 'There was an error loading data...',
-      });
-    }
-  }
+      dispatch({ type: 'loading' });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: 'city/deleted', payload: data });
+      } catch (err) {
+        dispatch({
+          type: 'cities/loaded',
+          payload: 'There was an error loading data...',
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: 'loading' });
