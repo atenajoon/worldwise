0843f40607f1c47df86b0089a9942ddd8e658db8
Atena Razm <atenarazm@gmail.com>
2024-08-19 13:30:20 -0700

    comment added to explain Outlet element

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,8 @@ import AppNav from './AppNav';
 import styles from './Sidebar.module.css';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
-
+// this Outlet element is just like the "children" props we pass to a component to read the child components nested in it
+// so, this Outlet is infact the nested routes of App route (cities, countries, form)
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
